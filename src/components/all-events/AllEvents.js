9c212a8e82ae059c53621962a979ea99d505e80e
Arunkumar S <iamsarunn03@gmail.com>
2024-03-02 22:37:03 +0530

    navbar changes

--- src/components/all-events/AllEvents.js
+++ src/components/all-events/AllEvents.js
@@ -125,6 +125,15 @@ export default function AllEvents() {
                     O<span className="text-black text-xl">ur Team</span>{" "}
                   </div>
                 </Link>
+                <Link
+                  to={"/contact-us"}
+                  className="flex text-lg px-4 py-4 items-center gap-3 transition-all duration-300 ease-in-out hover:bg-purple-300 "
+                >
+                  <VscOrganization size={20} />{" "}
+                  <div className="text-purple-500 text-2xl ">
+                    C<span className="text-black text-xl">ontact Us</span>{" "}
+                  </div>
+                </Link>
               </ul>
             </motion.div>
           )}
